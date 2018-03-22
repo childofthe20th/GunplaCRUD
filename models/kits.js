33d3b70454a19f9ecd6220240eaec3e79704e8c0
Eric Sanchez <bluemojo82@gmail.com>
2018-03-22 14:31:10 -0400

    testing anonymous feature

--- models/kits.js
+++ models/kits.js
@@ -3,7 +3,8 @@ const kitSchema = new mongoose.Schema({
     name: String,
     grade: String,
     img: [String],
-    description: String
+    description: String,
+    user: String
 });
 
 const Kit = mongoose.model('Kit', kitSchema);
